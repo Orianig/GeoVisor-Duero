500c4fcf6d2979b171778576fee741b2cd8c424e
orianig <oriana.ig016@gmail.com>
2025-10-09 23:36:18 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get('/health', (req, res) => {
   });
 });
 
-app.all('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).json({
     error: 'Route not found',
     message: `The route ${req.originalUrl} does not exist`,
